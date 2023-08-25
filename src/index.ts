46677a920c4ae95fb22162f0a48a08b03fd32ee5
mortimr <iulian@rotaru.fr>
2023-08-25 17:18:23 +0200

    feat: improve init

--- src/index.ts
+++ src/index.ts
@@ -148,6 +148,7 @@ program
 		await exec('rm -rf src test script');
 		await exec('git clone https://github.com/kilnfi/safecd-templates.git ./template');
 		await exec('cp -r ./template/script .');
+		await exec('cp -r ./template/.github .');
 		await exec('cp ./template/.gitignore .');
 		await exec('rm -rf template');
 		await exec('mkdir safes');
